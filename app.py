dadb283d80d1e4fc03582543c88d7f2ec5f89b1b
dyu2026 <derekyu@gmail.com>
2026-03-14 23:08:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ with tab2:
     st.dataframe(
         df_display.sort_values("first_seen_at", ascending=False)[display_cols],
         column_config={
-            "logo": st.column_config.ImageColumn("Logo", width="small"),
+            "logo": st.column_config.ImageColumn("Logo", width=35),
             "url": st.column_config.LinkColumn("Apply", display_text="Open"),
             "first_seen_at": "First Seen",
             "company": "Company",
@@ -379,7 +379,7 @@ with tab4:
         st.dataframe(
             removed_df.sort_values("first_seen_at", ascending=False)[safe_cols],
             column_config={
-                "logo": st.column_config.ImageColumn("Logo", width="small"), # Renders the image
+                "logo": st.column_config.ImageColumn("Logo", width=35),
                 "url": st.column_config.LinkColumn("Apply", display_text="Open"),
                 "first_seen_at": "First Seen",
                 "company": "Company",
