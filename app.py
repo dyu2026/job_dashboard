2a356aba0cd6b4519bd8506d8dcd7bd062442ffb
dyu2026 <derekyu@gmail.com>
2026-04-08 18:43:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ with st.sidebar:
 #st.title("Job Intelligence Dashboard")
 st.markdown("""
 <h1 style="margin-bottom: 0;">Job Intelligence Dashboard</h1>
-<p style="color: gray; margin-bottom: 30px;">
+<p style="color: gray; margin-bottom: 40px;">
 Track Japan & remote tech jobs with real-time hiring insights
 </p>
 """, unsafe_allow_html=True)
