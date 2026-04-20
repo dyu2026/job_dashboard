848f2936a6273a9cff01c52b69125c67d98dc230
dyu2026 <derekyu@gmail.com>
2026-04-20 18:39:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -857,6 +857,16 @@ with tab3:
     # CASE 2: Default → Company breakdown
     # -----------------------------------
     else:
+        
+        st.markdown("""
+            <style>
+            .stAlert {
+                background-color: #f5f7fb; /* Custom background */
+                color: #33394c;            /* Custom text color */
+            }
+            </style>
+            """, unsafe_allow_html=True)
+        
         st.info("**Tip:** Select a single company in the sidebar filter to view its detailed role breakdown.")
         company_stats = (
             df_company.groupby("company")
