44e01cd428a6960d2bfd282b35bb026a198978db
dyu2026 <derekyu@gmail.com>
2026-04-21 15:50:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ JST = timezone(timedelta(hours=9))
 # -----------------------------------
 
 with st.sidebar:
+    st.image("logos/RainbowDino_200x173.png", width=140)
     st.markdown("""
     <style>
     .sidebar-title {
