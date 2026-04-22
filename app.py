896b17ccb12a8277db54d2e2a5de5db69588a954
dyu2026 <derekyu@gmail.com>
2026-04-23 08:52:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -482,7 +482,7 @@ if not df.empty and "last_seen_at" in df.columns:
 
     # 7. Display
     st.sidebar.markdown("---")
-    st.sidebar.caption(f"🟢 Last updated: {rel} ({absolute})")
+    st.sidebar.caption(f"Last updated: {rel} ({absolute})")
     st.sidebar.caption("Made in :streamlit: by [Derek Yu](https://www.linkedin.com/in/derekhyyu/)")
 
 # -----------------------------------
