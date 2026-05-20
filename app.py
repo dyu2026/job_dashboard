1148d22e07081aa406b7b119080f4e9c1ea841af
dyu2026 <derekyu@gmail.com>
2026-05-20 12:16:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -607,11 +607,6 @@ now_utc = pd.Timestamp.now(tz="UTC")
 now_jst = pd.Timestamp.now(tz="Asia/Tokyo")
 last_24_utc = now_utc - pd.Timedelta(hours=24)
 
-#debug_active
-st.write("selected_roles:", selected_roles)
-st.write("selected_seniority:", selected_seniority)
-st.write("selected_companies:", selected_companies)
-st.write("search:", repr(st.session_state.search))
 
 # -----------------------------------
 # Sidebar Filters
@@ -730,6 +725,14 @@ if not df.empty and "last_seen_at" in df.columns:
     st.sidebar.markdown("---")
     st.sidebar.caption(f"Updated: {rel} ({absolute})")
     st.sidebar.caption("Made in :streamlit: by [Derek Yu](https://www.linkedin.com/in/derekhyyu/)")
+
+
+#debug_active
+st.write("selected_roles:", selected_roles)
+st.write("selected_seniority:", selected_seniority)
+st.write("selected_companies:", selected_companies)
+st.write("search:", repr(st.session_state.search))
+
 
 # -----------------------------------
 # Apply Filters
